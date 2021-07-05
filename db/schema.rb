c11622a7eee4402403c9919686b6628293966502
EC2 Default User <ec2-user@ip-172-31-86-49.ec2.internal>
2021-07-05 14:31:17 +0000

    [add]orde&order_item Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_092417) do
+ActiveRecord::Schema.define(version: 2021_07_05_141320) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,6 +61,26 @@ ActiveRecord::Schema.define(version: 2021_07_05_092417) do
     t.integer "admin_id"
     t.boolean "is_active", default: true, null: false
     t.index ["admin_id"], name: "index_items_on_admin_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "costomer_id"
+    t.string "postal_code"
+    t.string "address"
+    t.string "name"
+    t.integer "price"
+    t.integer "shipping_cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
